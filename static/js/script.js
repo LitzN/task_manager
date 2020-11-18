d3c20211518172300623ae0792eebf15cabf0d91
LitzN <alitzianedyalkova@hotmail.co.uk>
2020-11-18 20:09:49 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,13 @@
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
     $('select').formSelect();
+    $('.datepicker').datepicker({
+        format:"dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done:"Select"
+        }
+    }
+    );
   });
